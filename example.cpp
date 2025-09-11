43b757d84d4e8a0b09b6c9aefb60ce615469f22b
Eduardo Costa <m4c0@users.noreply.github.com>
2025-09-11 15:55:22 +0100

    rewrite example with my own libs

--- example.cpp
+++ example.cpp
@@ -6,32 +6,23 @@ extern "C" {
 const TSLanguage *tree_sitter_java();
 }
 
-#include <stdio.h>
+import hay;
+import jojo;
+import print;
 
 void dump(TSNode node, unsigned indent) {
-  printf("%*s%s %s\n", indent, "", ts_node_grammar_type(node),
-         ts_node_string(node));
+  putfn("%*s%s", indent, "", ts_node_grammar_type(node));
   for (auto i = 0; i < ts_node_child_count(node); i++) {
     dump(ts_node_child(node, i), indent + 2);
   }
 }
 
 int main() {
-  TSParser * parser = ts_parser_new();
+  hay<TSParser *, ts_parser_new, ts_parser_delete> parser {};
   ts_parser_set_language(parser, tree_sitter_java());
 
-  TSInput input{
-      .payload = fopen("test.java", "rb"),
-      .read = [](void *payload, uint32_t index, TSPoint pos,
-                 uint32_t *bytes) -> const char * {
-        static char buf[10240]{};
-        FILE *file = static_cast<FILE *>(payload);
-        *bytes = fread(buf, 1, sizeof(buf), file);
-        return buf;
-      },
-      .encoding = TSInputEncodingUTF8,
-  };
-  TSTree *tree = ts_parser_parse(parser, nullptr, input);
+  auto src = jojo::read_cstr("test.java");
+  TSTree *tree = ts_parser_parse_string(parser, nullptr, src.begin(), src.size());
   TSNode node = ts_tree_root_node(tree);
   dump(node, 0);
 }
